03de0ae6b1a1824422336a504c91cf34a2b49c6c
Malcom Musonza <malcommusonzasf@gmail.com>
2024-08-06 10:44:29 +0200

    Initial implementation of Desktop Left Sidebar

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,7 +2,7 @@ import HeaderBox from '@/components/HeaderBox'
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
-  const loggedIn = {firstName: 'Username'};
+  const loggedIn = {firstName: 'user_First_Name'};
   return (
     <section className="home">
      <div className="home-content">
